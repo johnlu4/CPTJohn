3a4052a8c128ce420199c20ff6aca91732754af8
johnlu4 <jiazhong.lu26@ycdsbk12.ca>
2025-01-10 09:42:10 -0500

    update v1.13
    some progress on the scrambling method and some variable fix to be fitting for cadawas' style

--- FinalCPTJohn.java
+++ FinalCPTJohn.java
@@ -27,48 +27,50 @@ public class FinalCPTJohn{
 		strSelectedTheme = con.readLine();
 		
 		TextInputFile theme = new TextInputFile(strSelectedTheme+".txt");
-		int WordsLength = 0;
+		int intWordsLength = 0;
 		
 		while(theme.eof() == false){
 			theme.readLine();
-			WordsLength += 1;
+			intWordsLength += 1;
 		}
 		
-		while(WordsLength == 0){
+		while(intWordsLength == 0){
 			con.println("Invalid theme, please choose a theme you want to play.");
 			strSelectedTheme = con.readLine();
 			theme = new TextInputFile(strSelectedTheme+".txt");
 			while(theme.eof() == false){
 				theme.readLine();
-				WordsLength += 1;
+				intWordsLength += 1;
 			}
 		}
 		
 		theme.close();
 		
 		theme = new TextInputFile(strSelectedTheme+".txt");
-		String WordsArray[][] = new String[WordsLength][2];
+		String strWordsArray[][] = new String[intWordsLength][2];
 		int intLength = 0;
 		
 		while(theme.eof() == false){
-			WordsArray[intLength][0] = theme.readLine();
+			strWordsArray[intLength][0] = theme.readLine();
 			intLength += 1;
 		}
 		
 		int intCount;
 		
-		for(intCount = 0; intCount < WordsLength; intCount++){
+		for(intCount = 0; intCount < intWordsLength; intCount++){
 			int intRand = (int)(Math.random() * 100 + 1);
-			WordsArray[intCount][1] = String.valueOf(intRand);
+			strWordsArray[intCount][1] = String.valueOf(intRand);
 		} 
 		
-		String strSecret = GetRandomized(WordsArray);
-		String strScrambledWord = new String[strSecret.length()][2];
+		String strSecret = GetRandomized(strWordsArray, intWordsLength);
+		
+		
+		String strScrambledWord = ScrambleString(con, strSecret, strSecret.length());
 		int intTries = strSecret.length() - 4;
 		boolean boolGuessed = false;
 		String strGuessedWord;
 		
-		con.println(strSelectedWord);
+		con.println(strSecret + strScrambledWord);
 		if(strUsername.toLowerCase().equals("statitan")){
 			intTries += 2;
 			con.println("You have "+intTries+" Tries. (+2 more with 'advantage')");
@@ -91,57 +93,36 @@ public class FinalCPTJohn{
 		}
 	}
 	
-	public static String ScrambleArrayIntoString  (String array2D[][]){
-		String strScrambledWord = "":
-		
-		String strTempTheme;
-		String strTempOrder;
-		int intRow2;
-		int intRow;
-		
-		for(intRow2 = 0; intRow2 < 10-1; intRow2++){
-			for(intRow = 0; intRow < 10-1-intRow2; intRow++){
-			// Bubble sort. If left is bigger than right
-			if(Integer.parseInt(array2D[intRow][1]) > Integer.parseInt(array2D[intRow+1][1])){
-				// Take the left item
-				strTempTheme = array2D[intRow][0];
-				strTempOrder = array2D[intRow][1];
-				// Right item moves to the left
-				array2D[intRow][0] = array2D[intRow + 1][0];
-				array2D[intRow][1] = array2D[intRow + 1][1];
-				// Put temporary value on the right.
-				array2D[intRow+1][0] = strTempTheme;
-				array2D[intRow+1][1] = strTempOrder;
-				}
-			}
-		}
+	public static String ScrambleString(Console con, String word, int intLength){
+		char[] charWordArray = word.toCharArray();
 		
 		
 		
 		return "Yes";
 	}
 	
-	public static String GetRandomized(String array2D[][]){
+	public static String GetRandomized(String strArray2D[][], int intWordLength){
 		String strTempTheme;
 		String strTempOrder;
 		int intRow2;
 		int intRow;
-		for(intRow2 = 0; intRow2 < 10-1; intRow2++){
-			for(intRow = 0; intRow < 10-1-intRow2; intRow++){
+		for(intRow2 = 0; intRow2 < intWordLength-1; intRow2++){
+			for(intRow = 0; intRow < intWordLength-1-intRow2; intRow++){
 			// Bubble sort. If left is bigger than right
-			if(Integer.parseInt(array2D[intRow][1]) > Integer.parseInt(array2D[intRow+1][1])){
+			if(Integer.parseInt(strArray2D[intRow][1]) > Integer.parseInt(strArray2D[intRow+1][1])){
 				// Take the left item
-				strTempTheme = array2D[intRow][0];
-				strTempOrder = array2D[intRow][1];
+				strTempTheme = strArray2D[intRow][0];
+				strTempOrder = strArray2D[intRow][1];
 				// Right item moves to the left
-				array2D[intRow][0] = array2D[intRow + 1][0];
-				array2D[intRow][1] = array2D[intRow + 1][1];
+				strArray2D[intRow][0] = strArray2D[intRow + 1][0];
+				strArray2D[intRow][1] = strArray2D[intRow + 1][1];
 				// Put temporary value on the right.
-				array2D[intRow+1][0] = strTempTheme;
-				array2D[intRow+1][1] = strTempOrder;
+				strArray2D[intRow+1][0] = strTempTheme;
+				strArray2D[intRow+1][1] = strTempOrder;
 				}
 			}
 		}
-		return array2D[0][0];
+	
+		return strArray2D[0][0];
 	}
 }
