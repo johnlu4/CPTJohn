06bf85f1199468e661067d898746eadc05a14b05
johnlu4 <jiazhong.lu26@ycdsbk12.ca>
2025-01-17 09:34:16 -0500

    update v1.16
    working menu system, progress with help menu and working guess the word system.

--- FinalCPTJohn.java
+++ FinalCPTJohn.java
@@ -8,11 +8,13 @@ public class FinalCPTJohn{
 		//con.setBackgroundColor(Color.WHITE);
 		//con.setTextColor(Color.BLACK);
 		TextInputFile themes = new TextInputFile("themes.txt");
+		BufferedImage imgGuesstheWord = con.loadImage("GuesstheWord.png");
 		
 		String strUsername;
 		String strSelectedTheme;
-		String strMenu;
+		String strMenu = "MainMenu";
 		Boolean boolAdvantage = false;
+		int intLoopedTwice = 0;
 		
 		con.println("What is your name?");
 		strUsername = con.readLine();
@@ -21,120 +23,153 @@ public class FinalCPTJohn{
 		
 		con.clear();
 		
-		BufferedImage imgGuesstheWord = con.loadImage("GuesstheWord.png");
+		while(!strMenu.equals("Exit")){
+			if(strMenu.equals("MainMenu")){
+				con.println("Hi");
+				con.drawImage(imgGuesstheWord, 440, 25);
+				con.drawString("(P)lay", 285,420);
+				con.drawString("(H)elp", 535,520);
+				con.drawString("(E)xit", 785,420);
+				con.drawRoundRect(250, 400, 200, 70, 9, 9);
+				con.drawRoundRect(500, 500, 200, 70, 9, 9);
+				con.drawRoundRect(750, 400, 200, 70, 9, 9);
+				
+				if(intLoopedTwice != 1){
+					intLoopedTwice += 1;
+				}else{
+					char getChar = con.getChar();
+					if(getChar == 'e'){
+						con.closeConsole();
+					}else if(getChar == 'h'){
+						strMenu = "HelpMenu";
+						con.clear();
+					}else if(getChar == 'p'){
+						strMenu = "PlayMenu";
+					}
+				}
+				
+			
+			}else if(strMenu.equals("HelpMenu")){			
+				con.println("Help Menu: ");
+				
+			}else if(strMenu.equals("PlayMenu")){
+				while(themes.eof() == false){
+					con.println(themes.readLine());
+					//ThemeLength += 1;
+				}
 		
-		con.drawImage(imgGuesstheWord, 440, 25);
-		con.drawString("(P)lay", 285,420);
-		con.drawString("(H)elp", 535,520);
-		con.drawString("(E)xit", 785,420);
-		con.drawRoundRect(250, 400, 200, 70, 9, 9);
-		con.drawRoundRect(500, 500, 200, 70, 9, 9);
-		con.drawRoundRect(750, 400, 200, 70, 9, 9);
-		while(themes.eof() == false){
-			con.println(themes.readLine());
-			//ThemeLength += 1;
-		}
+				themes.close();
 		
-		themes.close();
+				con.println("Which theme do you want to play?");
+				strSelectedTheme = con.readLine();
 		
-		con.println("Which theme do you want to play?");
-		strSelectedTheme = con.readLine();
+				TextInputFile theme = new TextInputFile(strSelectedTheme+".txt");
+				int intWordsLength = 0;
 		
-		TextInputFile theme = new TextInputFile(strSelectedTheme+".txt");
-		int intWordsLength = 0;
+				while(theme.eof() == false){
+					theme.readLine();
+					intWordsLength += 1;
+				}
 		
-		while(theme.eof() == false){
-			theme.readLine();
-			intWordsLength += 1;
-		}
+				while(intWordsLength == 0){
+					con.println("Invalid theme, please choose a theme you want to play.");
+					strSelectedTheme = con.readLine();
+					theme = new TextInputFile(strSelectedTheme+".txt");
+					while(theme.eof() == false){
+						theme.readLine();
+						intWordsLength += 1;
+					}
+				}
 		
-		while(intWordsLength == 0){
-			con.println("Invalid theme, please choose a theme you want to play.");
-			strSelectedTheme = con.readLine();
-			theme = new TextInputFile(strSelectedTheme+".txt");
-			while(theme.eof() == false){
-				theme.readLine();
-				intWordsLength += 1;
+				theme.close();
+		
+				theme = new TextInputFile(strSelectedTheme+".txt");
+				String strWordsArray[][] = new String[intWordsLength][2];
+				
+				int intLength = 0;
+		
+				while(theme.eof() == false){
+					strWordsArray[intLength][0] = theme.readLine();
+					intLength += 1;
+				}
+		
+				int intCount;
+		
+				for(intCount = 0; intCount < intWordsLength; intCount++){
+					int intRand = (int)(Math.random() * 100 + 1);
+					strWordsArray[intCount][1] = String.valueOf(intRand);
+				} 
+		
+		
+				String strSecret = GetRandomized(strWordsArray, intWordsLength);
+				String strSecretWordArray[][];
+				if(strSecret.contains(" ")){
+					String[] strSplitWordArray = strSecret.split(" ");
+					strSecretWordArray = new String[strSecret.length()][2];
+		
+					for(intCount = 0; intCount < strSecret.length(); intCount++){
+						strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
+						System.out.println(strSecretWordArray[intCount][0]);
+					} 
+		
+					for(intCount = 0; intCount < strSecret.length(); intCount++){
+						int intRand = (int)(Math.random() * 100 + 1);
+						strSecretWordArray[intCount][1] = String.valueOf(intRand);
+						System.out.println(strSecretWordArray[intCount][1]);
+					} 	
+				}else{
+					strSecretWordArray = new String[strSecret.length()][2];
+				
+					for(intCount = 0; intCount < strSecret.length(); intCount++){
+						strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
+						System.out.println(strSecretWordArray[intCount][0]);
+					} 
+		
+					for(intCount = 0; intCount < strSecret.length(); intCount++){
+						int intRand = (int)(Math.random() * 100 + 1);
+						strSecretWordArray[intCount][1] = String.valueOf(intRand);
+						System.out.println(strSecretWordArray[intCount][1]);
+					} 	
+				}
+		
+				String strScrambledWord = ScrambleString(strSecretWordArray, strSecret.length());
+				System.out.println(strScrambledWord);
+				int intTries = strSecret.length() - 4;
+				boolean boolGuessed = false;
+				String strGuessedWord;
+		
+				con.println(strScrambledWord);
+				if(strUsername.toLowerCase().equals("statitan")){
+					intTries += 2;
+					con.println("You have "+intTries+" Tries. (+2 more with 'advantage')");
+				}else{
+					con.println("You have "+intTries+" Tries. ");
+				}
+				while(intTries > 0 && boolGuessed == false){
+					strGuessedWord = con.readLine();
+					if(strGuessedWord.toLowerCase().equals(strSecret.toLowerCase())){
+						boolGuessed = true;
+					}else{
+						intTries -= 1;
+						con.println("Wrong, you have " + intTries + " Tries left, guess again.");
+					}
+				}
+				if(boolGuessed == true){
+					con.println("You guessed it right!");
+				}else{
+					con.println("You suck balls, the word was: "+strSecret+".");
+				}
+				con.println("Would you like to (p)lay again, (r)eturn to main menu or (e)xit?");
+				char getChar = con.getChar();
+				if(getChar == 'e'){
+					con.closeConsole();
+				}else if(getChar == 'p'){
+					strMenu = "PlayMenu";
+					con.clear();
+				}else if(getChar == 'r'){
+					strMenu = "MainMenu";
+				}
 			}
-		}
-		
-		theme.close();
-		
-		theme = new TextInputFile(strSelectedTheme+".txt");
-		String strWordsArray[][] = new String[intWordsLength][2];
-		
-		int intLength = 0;
-		
-		while(theme.eof() == false){
-			strWordsArray[intLength][0] = theme.readLine();
-			intLength += 1;
-		}
-		
-		int intCount;
-		
-		for(intCount = 0; intCount < intWordsLength; intCount++){
-			int intRand = (int)(Math.random() * 100 + 1);
-			strWordsArray[intCount][1] = String.valueOf(intRand);
-		} 
-		
-		
-		String strSecret = GetRandomized(strWordsArray, intWordsLength);
-		String strSecretWordArray[][];
-		if(strSecret.contains(" ")){
-			String[] strSplitWordArray = strSecret.split(" ");
-			strSecretWordArray = new String[strSecret.length()][2];
-		
-			for(intCount = 0; intCount < strSecret.length(); intCount++){
-				strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
-				System.out.println(strSecretWordArray[intCount][0]);
-			} 
-		
-			for(intCount = 0; intCount < strSecret.length(); intCount++){
-				int intRand = (int)(Math.random() * 100 + 1);
-				strSecretWordArray[intCount][1] = String.valueOf(intRand);
-				System.out.println(strSecretWordArray[intCount][1]);
-			} 	
-		}else{
-			strSecretWordArray = new String[strSecret.length()][2];
-		
-			for(intCount = 0; intCount < strSecret.length(); intCount++){
-				strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
-				System.out.println(strSecretWordArray[intCount][0]);
-			} 
-		
-			for(intCount = 0; intCount < strSecret.length(); intCount++){
-				int intRand = (int)(Math.random() * 100 + 1);
-				strSecretWordArray[intCount][1] = String.valueOf(intRand);
-				System.out.println(strSecretWordArray[intCount][1]);
-			} 	
-		}
-		
-		String strScrambledWord = ScrambleString(strSecretWordArray, strSecret.length());
-		System.out.println(strScrambledWord);
-		int intTries = strSecret.length() - 4;
-		boolean boolGuessed = false;
-		String strGuessedWord;
-		
-		con.println(strScrambledWord);
-		if(strUsername.toLowerCase().equals("statitan")){
-			intTries += 2;
-			con.println("You have "+intTries+" Tries. (+2 more with 'advantage')");
-		}else{
-			con.println("You have "+intTries+" Tries. ");
-		}
-		while(intTries > 0 && boolGuessed == false){
-			strGuessedWord = con.readLine();
-			if(strGuessedWord.toLowerCase().equals(strSecret.toLowerCase())){
-				boolGuessed = true;
-			}else{
-				intTries -= 1;
-				con.println("Wrong, you have " + intTries + " Tries left, guess again.");
-			}
-		}
-		if(boolGuessed == true){
-			con.println("You guessed it right!");
-		}else{
-			con.println("You suck balls, the word was: "+strSecret+".");
 		}
 	}
 	
