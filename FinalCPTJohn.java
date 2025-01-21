0432a557b8bfb3f3fb8624ef16c3ed30ef40ac12
johnlu4 <jiazhong.lu26@ycdsbk12.ca>
2025-01-21 09:32:52 -0500

    final update (v1.17)
    added highscore, fixed play menu, improved play menu screen, added more themes

--- FinalCPTJohn.java
+++ FinalCPTJohn.java
@@ -5,9 +5,22 @@ import java.awt.image.*;
 public class FinalCPTJohn{
 	public static void main(String[] args){
 		Console con = new Console("Guess the word", 1280,720);
-		//con.setBackgroundColor(Color.WHITE);
-		//con.setTextColor(Color.BLACK);
+		
 		BufferedImage imgGuesstheWord = con.loadImage("GuesstheWord.png");
+		TextInputFile HighscoreInput = new TextInputFile("highscore.txt");
+		
+		int intHighscore = 0;
+		int intSavedHighscore = 0;
+		
+		while(HighscoreInput.eof() == false){
+			int intGetHighscore = HighscoreInput.readInt();
+			if(intGetHighscore > intSavedHighscore){
+				intSavedHighscore = intGetHighscore;
+			};
+		}
+		
+		HighscoreInput.close();
+		
 		
 		String strUsername;
 		String strSelectedTheme;
@@ -27,6 +40,8 @@ public class FinalCPTJohn{
 				con.println(" ");
 				con.drawImage(imgGuesstheWord, 440, 25);
 				con.drawString("Welcome, "+strUsername+"!", 20,675);
+				con.drawString("Current Highscore: "+intHighscore, 900,675);
+				con.drawString("Highscore Record: "+intSavedHighscore, 900,650);
 				con.drawString("(P)lay", 285,420);
 				con.drawString("(H)elp", 535,520);
 				con.drawString("(E)xit", 785,420);
@@ -39,6 +54,7 @@ public class FinalCPTJohn{
 				}else{
 					char getChar = con.getChar();
 					if(getChar == 'e'){
+						System.out.println("Exitted");
 						con.closeConsole();
 					}else if(getChar == 'h'){
 						strMenu = "HelpMenu";
@@ -46,12 +62,17 @@ public class FinalCPTJohn{
 						con.setDrawColor(Color.BLACK);
 						con.fillRect(0,0,1280,720);
 						con.setDrawColor(Color.WHITE);
+						System.out.println("HelpMenu");
 					}else if(getChar == 'p'){
 						strMenu = "PlayMenu";
 						con.clear();
 						con.setDrawColor(Color.BLACK);
 						con.fillRect(0,0,1280,720);
 						con.setDrawColor(Color.WHITE);
+						System.out.println("PlayMenu");
+					}else if(getChar == 's'){
+						con.drawString("What do computers like to eat? chips.", 400, 350);
+						System.out.println("Secret Joke");
 					}
 				}
 				
@@ -65,11 +86,13 @@ public class FinalCPTJohn{
 				con.println("If you lose all your tries its game over.");
 				con.println("You have tries calculated by how many letters are in the word - 4");
 				con.println("You will have a highscore depending on how much you win.");
+				con.println("If you lose your highscore is back to 0");
 				con.println("Press any key to return to the main menu.");
 				
 				char getChar = con.getChar();
 				strMenu = "MainMenu";
 				con.clear();
+				System.out.println("Returned to main menu");
 			}else if(strMenu.equals("PlayMenu")){
 				TextInputFile themes = new TextInputFile("themes.txt");
 				while(themes.eof() == false){
@@ -111,72 +134,79 @@ public class FinalCPTJohn{
 					strWordsArray[intLength][0] = theme.readLine();
 					intLength += 1;
 				}
-		
+				theme.close();
 				int intCount;
 		
 				for(intCount = 0; intCount < intWordsLength; intCount++){
 					int intRand = (int)(Math.random() * 100 + 1);
 					strWordsArray[intCount][1] = String.valueOf(intRand);
 				} 
-		
+				
+				con.clear();
 		
 				String strSecret = GetRandomized(strWordsArray, intWordsLength);
 				String strSecretWordArray[][];
-				if(strSecret.contains(" ")){
-					String[] strSplitWordArray = strSecret.split(" ");
-					strSecretWordArray = new String[strSecret.length()][2];
-		
-					for(intCount = 0; intCount < strSecret.length(); intCount++){
-						strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
-						System.out.println(strSecretWordArray[intCount][0]);
-					} 
-		
-					for(intCount = 0; intCount < strSecret.length(); intCount++){
-						int intRand = (int)(Math.random() * 100 + 1);
-						strSecretWordArray[intCount][1] = String.valueOf(intRand);
-						System.out.println(strSecretWordArray[intCount][1]);
-					} 	
-				}else{
-					strSecretWordArray = new String[strSecret.length()][2];
+				System.out.println(strSecret);
 				
-					for(intCount = 0; intCount < strSecret.length(); intCount++){
-						strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
-						System.out.println(strSecretWordArray[intCount][0]);
-					} 
+				strSecretWordArray = new String[strSecret.length()][2];
+				
+				for(intCount = 0; intCount < strSecret.length(); intCount++){
+					strSecretWordArray[intCount][0] = Character.toString(strSecret.charAt(intCount));
+				} 
 		
-					for(intCount = 0; intCount < strSecret.length(); intCount++){
-						int intRand = (int)(Math.random() * 100 + 1);
-						strSecretWordArray[intCount][1] = String.valueOf(intRand);
-						System.out.println(strSecretWordArray[intCount][1]);
-					} 	
-				}
+				for(intCount = 0; intCount < strSecret.length(); intCount++){
+					int intRand = (int)(Math.random() * 100 + 1);
+					strSecretWordArray[intCount][1] = String.valueOf(intRand);
+				} 	
 		
 				String strScrambledWord = ScrambleString(strSecretWordArray, strSecret.length());
-				System.out.println(strScrambledWord);
 				int intTries = strSecret.length() - 4;
 				boolean boolGuessed = false;
 				String strGuessedWord;
-		
-				con.println(strScrambledWord);
+				
+				con.drawRect(500, 250, 240, 70);
+				con.drawString("Theme: "+strSelectedTheme, 530, 210);
+				con.drawString(strScrambledWord, 550,270);
 				if(strUsername.toLowerCase().equals("statitan")){
 					intTries += 2;
 					con.println("You have "+intTries+" Tries. (+2 more with 'advantage')");
 				}else{
 					con.println("You have "+intTries+" Tries. ");
 				}
+				
+				con.drawString("Tries: "+intTries, 900,675);
+				con.drawString("Current Highscore: "+intHighscore, 20,675);
+				
 				while(intTries > 0 && boolGuessed == false){
 					strGuessedWord = con.readLine();
 					if(strGuessedWord.toLowerCase().equals(strSecret.toLowerCase())){
 						boolGuessed = true;
 					}else{
 						intTries -= 1;
-						con.println("Wrong, you have " + intTries + " Tries left, guess again.");
+						con.println("Wrong, guess again.");
+						con.setDrawColor(Color.BLACK);
+						con.fillRect(865,625,1280,720);
+						con.setDrawColor(Color.WHITE);
+						con.drawString("Tries: "+intTries, 900,675);
 					}
 				}
 				if(boolGuessed == true){
+					con.clear();
 					con.println("You guessed it right!");
+					intHighscore += 1;
+					con.setDrawColor(Color.BLACK);
+					con.fillRect(40,655,400,300);
+					con.setDrawColor(Color.WHITE);
+					con.drawString("Current Highscore: "+intHighscore, 20,675);
+					if(intHighscore > intSavedHighscore){
+						intSavedHighscore = intHighscore;
+						TextOutputFile HighscoreOutput = new TextOutputFile("highscore.txt");
+						HighscoreOutput.println(intHighscore);
+					}
 				}else{
-					con.println("You suck balls, the word was: "+strSecret+".");
+					con.clear();
+					intHighscore = 0;
+					con.println("You lose, the word was: "+strSecret+".");
 				}
 				con.println("Would you like to (p)lay again, (r)eturn to main menu or (e)xit?");
 				char getChar = con.getChar();
@@ -189,6 +219,12 @@ public class FinalCPTJohn{
 					con.fillRect(0,0,1280,720);
 					con.setDrawColor(Color.WHITE);
 				}else if(getChar == 'r'){
+					strMenu = "MainMenu";
+					con.clear();
+					con.setDrawColor(Color.BLACK);
+					con.fillRect(0,0,1280,720);
+					con.setDrawColor(Color.WHITE);
+				}else{
 					strMenu = "MainMenu";
 					con.clear();
 					con.setDrawColor(Color.BLACK);
